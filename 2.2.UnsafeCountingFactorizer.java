b0883806b752aac97af883ecb3ac0e94bda1a41c
Hoesung Jung <jrps2212@gmail.com>
2021-07-23 17:22:23 +0900

    implement 설명

--- 2.2.UnsafeCountingFactorizer.java
+++ 2.2.UnsafeCountingFactorizer.java
@@ -14,3 +14,5 @@ public class UnsafeCountingFactorizer implements Servlet {
 }
 
 // 경쟁 조건이 발생한다. 
+// 자바는 다중 상속을 지원하지 않는다. 대신 implements(인터페이스)가 등장했다. 
+// implements의 가장 큰 특징은 부모의 메소드를 반드시 오버라이딩(재정의)해야 한다. 
